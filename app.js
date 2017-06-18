9ccdb9002841640dd566cdb70dbe4b478aabdcbf
alanionita <a.g.ionita@gmail.com>
2017-06-18 20:53:39 +0100

    [bot] got a vertical slice to work for 'kenya' message triggers

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const { makeWatsiRequest } = require(path.resolve(__dirname, 'watsi', 'index'));
+const { sortByCountry } = require(path.resolve(__dirname, 'watsi', 'methods'));
 
 const Botmaster = require('botmaster');
 const botmaster = new Botmaster();
@@ -24,15 +27,31 @@ let myIncomingMiddlewareController = (bot, update) => {
     } else if (update.message.text.indexOf('kenya') > -1) {
         return new Promise((resolve, reject) => {
             if (update.message.text.indexOf('kenya') < -1) return reject('Country was not found');
-            return resolve(
-                bot.sendTextMessageTo('It is currently sunny in Kenya', update.sender.id)
-            );
-    });
-} else {
+            return makeWatsiRequest()
+                .then((patients) => {
+                    const filteredPatients = sortByCountry(patients.profiles, 'Kenya');
+                    return filteredPatients;
+                })
+                .then((filteredPatients) => {
+                    if (!filteredPatients) return reject(
+                        bot.reply(update, 'Sorry we don\'t have any treatment available in that country')
+                    )
+                    const firstPatient = filteredPatients.slice(0, 20);
+                    const messages = [
+                        'You can contribute to ' + firstPatient[0].name,
+                        firstPatient[0].header,
+                        firstPatient[0].url
+                    ];
+                    return resolve(
+                        bot.sendTextCascadeTo(messages, update.sender.id)
+                    );
+                })
+        });
+    } else {
         const messages = ['I\'m sorry about this.',
-    'But it seems like I couldn\'t understand your message.',
-    'Could you try reformulating it?']
-        return bot.sendTextCascadeTo(messages, update.sender.id)
+            'But it seems like I couldn\'t understand your message.',
+            'Could you try reformulating it?']
+        return bot.sendTextCascadeTo(messages, update.sender.id);
     }
 };
 
