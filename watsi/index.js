fbbcd28c8ea470651ebe760f098f4c73a7b27648
alanionita <a.g.ionita@gmail.com>
2017-06-18 23:33:13 +0100

    [watsi] added the code that was missing, promisified the request to Watsi

--- watsi/index.js
+++ watsi/index.js
@@ -1,21 +1,12 @@
 const request = require('request');
-const path = require('path');
 
-const URL = 'https://watsi.org/fund-treatments.json';
-const { sortByCountry, showOnlyTheFirstXEntries } = require(path.resolve(__dirname, 'methods'));
-
-exports.call = request.get(URL, {
-    json: true
-}, function (err, res, patients) {
+exports.makeWatsiRequest = () => {
     return new Promise((resolve, reject) => {
-        if (err) return reject(err);
-        return resolve(patients);
-    })
-        .then((patients) => {
-            const filteredByCountry = sortByCountry(patients.profiles, 'Kenya');
-            return filteredByCountry;
-        })
-        .then((filteredByCountry) => {
-            console.log(showOnlyTheFirstXEntries(filteredByCountry, 1));
+        request.get('https://watsi.org/fund-treatments.json', {
+            json: true
+        }, function (err, res, patients) {
+            if (err) return reject(err);
+            return resolve(patients);
         });
-});
+    });
+}; 
