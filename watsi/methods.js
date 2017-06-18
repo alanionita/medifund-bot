8d68b81267b363bcbd4fbf18c59c37933d14e198
alanionita <a.g.ionita@gmail.com>
2017-06-18 21:13:41 +0100

    [methods] added a random number generator

--- watsi/methods.js
+++ watsi/methods.js
@@ -9,3 +9,10 @@ exports.sortByCountry = (patientsArr, targetCountry) => {
 exports.showOnlyTheFirstXEntries = (patients, number) => {
     if (number && +number) return patients.slice(0, number);
 }
+
+exports.getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; 
+  // The maximum is exclusive and the minimum is inclusive
+};
