6aa993960cb4c6d2c5e0ede56167e6a1128f399f
alanionita <a.g.ionita@gmail.com>
2017-06-18 14:07:32 +0100

    [watsi] add model function to search for patients by country

--- watsi/index.js
+++ watsi/index.js
@@ -2,7 +2,24 @@ const request = require('request');
 const URL = 'https://watsi.org/fund-treatments.json';
 
 request.get(URL, {
-  json: true
-}, function(err, res, patients) {
-  console.log('Patients!', patients);
+    json: true
+}, function (err, res, patients) {
+    return new Promise((resolve, reject) => {
+        if (err) return reject(err);
+        return resolve(patients);
+    })
+        .then((patients) => {
+            const filteredByCountry = sortByCountry(patients, 'Kenya');
+            return filteredByCountry;
+        })
+        .then((filteredByCountry) => {
+            console.log(filteredByCountry.slice(0, 4));
+        });
 });
+
+
+function sortByCountry(patients, targetCountry) {
+    return patients.profiles.filter((patient) => {
+        if (patient.country === targetCountry) return patient;
+    });
+}
