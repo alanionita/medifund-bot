19b16659822d0aa6840fa011cae1a85231959569
alanionita <a.g.ionita@gmail.com>
2017-06-23 00:58:21 +0100

    [bot] refactored all of the messages into their own file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { makeWatsiRequest } = require(path.resolve(__dirname, 'watsi', 'index'));
 const { sortByCountry } = require(path.resolve(__dirname, 'watsi', 'methods'));
 const { getRandomInt } = require(path.resolve(__dirname, 'watsi', 'methods'));
+const messages = require(path.resolve(__dirname, 'messages', 'index'));
 
 const Botmaster = require('botmaster');
 const botmaster = new Botmaster();
@@ -20,14 +21,7 @@ let myIncomingMiddlewareController = (bot, update) => {
         update.message.text === 'yo' ||
         update.message.text === 'Hey' ||
         update.message.text === 'hey') {
-        const tutorial = ['Hey 👋',
-            'It\'s nice to meet you',
-            'This is a vertical slice for searching for treatments in Kenya',
-            'Try it out!',
-            'Hint: "I want to fund projects in Kenya"',
-        ];
-
-        return bot.sendTextCascadeTo(tutorial, update.sender.id);
+        return bot.sendTextCascadeTo(messages.tutorial, update.sender.id);
     } else if (update.message.text.indexOf('Kenya') > -1) {
         getPlacesICanHelp('Kenya')
             .then(message => {
@@ -42,12 +36,9 @@ let myIncomingMiddlewareController = (bot, update) => {
         update.message.text === 'Thank you' ||
         update.message.text === 'Thank you, very much' ||
         update.message.text === 'thank you very much') {
-        return bot.reply(update, 'Oh, I\'m blushing. I\'m glad you liked what you found');
+        return bot.sendTextCascadeTo(messages.thanksReply, update.sender.id);
     } else {
-        const appologies = ['I\'m sorry about this.',
-            'But it seems like I couldn\'t understand your message.',
-            'Could you try reformulating it?']
-        return bot.sendTextCascadeTo(appologies, update.sender.id);
+        return bot.sendTextCascadeTo(messages.appologies, update.sender.id);
     }
 };
 
@@ -59,7 +50,7 @@ function getPlacesICanHelp(place) {
                 return filteredPatients;
             })
             .then((filteredPatients) => {
-                if (!filteredPatients) reject('Sorry we don\'t have any treatment available in that country');
+                if (!filteredPatients) reject(messages.sorryCountry);
                 const firstPatient = filteredPatients.slice(0, 20);
                 let random = getRandomInt(0, 20);
                 const kenyaMessage = [
--- messages/index.js
+++ messages/index.js
@@ -1,3 +1,27 @@
 exports.appologies = [
+    'I\'m sorry about this.',
+    'But it seems like I couldn\'t understand your message.',
+    'Could you try reformulating it?'];
 
+exports.thanksReply = ['🙌 ',
+    'I\'m glad you liked what you found',
+    'do tell your friends about it',
+    'and check out',
+    'Watsi.com',
+    'they are a cool bunch',
+    '🤗'
 ];
+
+exports.tutorial = [
+    '👋',
+    'Hey',
+    'It\'s nice to meet you',
+    'Medi+Funder is just a prototype atm',
+    '🤓 : this is a vertical slice for searching for treatments in Kenya',
+    'Try it out!',
+    'Hint:', 
+    '"I want to fund projects in Kenya"',
+    '✌️'
+];
+
+exports.sorryCountry = 'Sorry we don\'t have any treatment available in that country';
